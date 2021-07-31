0e533f987bcda1e199dad1e03b3cf0cfe4228007
emerk777 <emerk777@yahoo.com>
2021-07-30 18:00:18 -0600

    Spring Context 32

--- Spring5DependencyInjection/src/main/java/com/example/SpringDependencyInjection/SpringDependencyInjectionApplication.java
+++ Spring5DependencyInjection/src/main/java/com/example/SpringDependencyInjection/SpringDependencyInjectionApplication.java
@@ -1,13 +1,21 @@
 package com.example.SpringDependencyInjection;
 
+import com.example.SpringDependencyInjection.Controller.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringDependencyInjectionApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringDependencyInjectionApplication.class, args);
+		ApplicationContext ctx = SpringApplication.run(SpringDependencyInjectionApplication.class, args);
+
+		MyController myController = (MyController) ctx.getBean("myController");
+
+		String greeting = myController.sayHello();
+
+		System.out.println(greeting);
 	}
 
 }
